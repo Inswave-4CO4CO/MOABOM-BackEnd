49be3864bbfae5cc63b6227aa96f416a239c22b7
milou <milouw56@gmail.com>
2025-05-14 11:21:02 +0900

    [feat] check id duplicate

--- src/main/java/com/moabom/backend/controller/AuthController.java
+++ src/main/java/com/moabom/backend/controller/AuthController.java
@@ -3,13 +3,11 @@ package com.moabom.backend.controller;
 import com.moabom.backend.constants.SecurityConstants;
 import com.moabom.backend.model.LoginRequest;
 import com.moabom.backend.model.SignupRequest;
+import com.moabom.backend.repository.UserRepository;
 import com.moabom.backend.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -21,13 +19,16 @@ import java.util.Map;
 public class AuthController {
 
     private final AuthService authService;
+    private final UserRepository userRepository;
 
+    // 회원가입
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody SignupRequest request) {
         authService.signup(request);
         return ResponseEntity.ok("회원가입 성공");
     }
 
+    // 로그인
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest request) {
         String token = authService.login(request);
@@ -35,5 +36,24 @@ public class AuthController {
         response.put(SecurityConstants.TOKEN_TYPE, SecurityConstants.TOKEN_PREFIX + token);
         return ResponseEntity.ok(response);
     }
+
+    // 사용자 인증
+    @GetMapping("/check")
+    public ResponseEntity<Map<String, String>> check(@RequestParam String token) {
+
+        return null;
+    }
+
+    // 아이디 중복 확인
+    @PostMapping("/checkId")
+    public ResponseEntity<?> checkUserId(@RequestBody Map<String, String> body) {
+        String userId = body.get("userId");
+        boolean exists = userRepository.existsByUserId(userId);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("available", !exists);
+        return ResponseEntity.ok(response);
+    }
+
 }
 
