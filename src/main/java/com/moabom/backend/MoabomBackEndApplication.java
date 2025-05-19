a9fcb51f0b5bd1ea0098c34873821acdce50e653
dorazi0423 <dorazi0423@gmail.com>
2025-05-19 10:13:44 +0900

    [feat] : 메인 머지

--- src/main/java/com/moabom/backend/MoabomBackEndApplication.java
+++ src/main/java/com/moabom/backend/MoabomBackEndApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoabomBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoabomBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoabomBackEndApplication.class, args);
+    }
 
 }
