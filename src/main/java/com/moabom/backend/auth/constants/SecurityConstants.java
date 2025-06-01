ba6e640f7282aac487b20b7006bd23aa419ef815
Seung Min Lee <milouw56@gmail.com>
2025-06-01 11:12:55 +0900

    [fix] access token tie

--- src/main/java/com/moabom/backend/auth/constants/SecurityConstants.java
+++ src/main/java/com/moabom/backend/auth/constants/SecurityConstants.java
@@ -8,6 +8,6 @@ public final class SecurityConstants {
     public static final String TOKEN_PREFIX="Bearer "; //Bearer공백${jwt}
 
     // 초 단위
-    public static final long ACCESS_TOKEN_EXPIRE = 60L ; // 30분 1분
+    public static final long ACCESS_TOKEN_EXPIRE = 60L * 60; // 30분 1분
     public static final long REFRESH_TOKEN_EXPIRE = 60L * 60 * 24 * 14; // 14일
 }
